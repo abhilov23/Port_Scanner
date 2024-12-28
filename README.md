66ed1df1b6c147ba7e7d0dff84bf1ccb55552574
Abhilov23 <abhilovgupta01@gmail.com>
2024-12-29 01:09:30 +0530

    updated readme.md file

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
-# Port_Scanner
-Here is a port scanner in Python.
+
+# Port Scanner
+
+**Port Scanner** is a Python script designed to identify open ports on a specified host. It utilizes the `socket` library to attempt connections to a range of ports and reports which ones are open.
+
+## Features
+
+- **Range Scanning**: Scan a specified range of ports on a target host.
+- **Customizable Timeouts**: Set connection timeout durations to balance between speed and accuracy.
+- **Threaded Scanning**: Employs multithreading to expedite the scanning process.
+
+## Requirements
+
+- Python 3.x
+
+## Installation
+
+1. **Clone the Repository**:
+   ```bash
+   git clone https://github.com/abhilov23/Port_Scanner.git
+   ```
+2. **Navigate to the Project Directory**:
+   ```bash
+   cd Port_Scanner
+   ```
+
+## Usage
+
+1. **Run the Script**:
+   ```bash
+   python port_scanner.py
+   ```
+2. **Provide Input When Prompted**:
+   - **Target Host**: Enter the IP address or domain name of the host you wish to scan.
+   - **Starting Port**: Enter the starting port number of the range you want to scan.
+   - **Ending Port**: Enter the ending port number of the range you want to scan.
+
+3. **Example**:
+   ```plaintext
+   Enter target host: example.com
+   Enter starting port: 20
+   Enter ending port: 80
+   ```
+
+   The script will then attempt to connect to each port in the specified range and display whether each port is open or closed.
+
+## Contributing
+
+Contributions are welcome! If you have suggestions for improvements or new features, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Disclaimer
+
+**Use this tool responsibly.** Unauthorized port scanning of systems without permission is illegal and unethical. Ensure you have explicit permission before scanning any network or host.
+
+## Author
+
+**Abhilov Gupta**  
+- GitHub: [abhilov23](https://github.com/abhilov23)  
+- Twitter: [@Abhilov_](https://twitter.com/Abhilov_)
--- port_scanner.py
+++ port_scanner.py
@@ -1,38 +1,40 @@
-import socket #using sockets
+import socket
 from IPy import IP
+
+def scan(target):
+    converted_ip = check_ip(target)
+    print('\n' + '[-_0 Scanning Target] ' + str(target))
+    for port in range(1, 100):
+        scan_port(converted_ip, port)
 
 def check_ip(ip):
     try:
-        IP(ip) #checking if it is a valid IP address
+        IP(ip)
         return(ip)
     except ValueError:
-        return socket.gethostbyname(ip) #trying to figure out the ip address of the website
+        return socket.gethostbyname(ip)
 
-def get_banner(s): #taking the socket object inside this function so that we can grab the services of the same.
-    return s.recv(2048) #receiving the socket data
+def get_banner(s):
+    return s.recv(1024)
 
-def port_scan(ipaddress, port):
+def scan_port(ipaddress, port):
     try:
-        second_sock = socket.socket() #using a separate variable for the second socket object
-        second_sock.settimeout(3)#setting up a timeout for fast scanning
-        second_sock.connect((ipaddress, port))
+        sock = socket.socket()
+        sock.settimeout(0.5)
+        sock.connect((ipaddress, port))
         try:
-            banner = get_banner(second_sock) #grabbing a banner out of no where
-            print('[+] Open Port '+ str(port) + ':' + str(banner.decode().strip('\n')))
+            banner = get_banner(sock)
+            print('[+] Open Port ' + str(port) + ' : ' + str(banner.decode().strip('\n')))
         except:
-            print('[+] Open Port '+ str(port))
+            print('[+] Open Port ' + str(port))
     except:
-        pass #this will not print anything and just scan another function for the same.
+        pass
 
-def scan(target): #for taking multiple targets and scan them accoudingly
-    converted_ip = check_ip(target)
-    print('\n' + '[- scanning Target]' + str(target))
-    for port in range(10, 90):
-        port_scan(converted_ip, port)
 
-targets = input('[+] Enter the target/s to scan: (split multiple targets with):') #taking input from users
-if ',' in targets: #if there is , in targets means use is specifying multiple targets otherwise not.
-    for ip_add in targets.split(','):
-        scan(ip_add.strip(' '))
-else:
-    scan(targets)
+if __name__ == "__main__":
+    targets = input('[+] Enter Target/s To Scan(split multiple targets with ,): ')
+    if ',' in targets:
+        for ip_add in targets.split(','):
+            scan(ip_add.strip(' '))
+    else:
+        scan(targets)
